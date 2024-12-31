3312da6badd339a76c03758cf4edd78c1c9c3fea
subaruya <s.subaruya@gmail.com>
2024-12-31 13:32:27 +0900

    config/environments/production.rb更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
   # ↑公開環境ではfalseに戻す
   config.action_controller.perform_caching = true
 
