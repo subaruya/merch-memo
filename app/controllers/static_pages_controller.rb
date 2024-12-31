cb53ed7ea990b2e53c8ed9f7cd6a5df53062394f
subaruya <s.subaruya@gmail.com>
2024-12-31 15:21:22 +0900

    トップページ作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
 class StaticPagesController < ApplicationController
-  def top; end
+  def top
+    render layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # get "/" => "home#top"
+  # get "/" => "home#top" 
   # Defines the root path route ("/")
   root "static_pages#top"
 end
