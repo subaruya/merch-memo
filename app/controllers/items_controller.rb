7ad3df5dabca60c9789beec686f6c4499f52d80d
subaruya <s.subaruya@gmail.com>
2025-01-03 18:16:55 +0900

    add:グッズ情報編集・削除用ルーティングを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,27 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item
+    .order(Arel.sql("COALESCE(release_date, '9999-12-31') ASC, pre_ordered_date ASC"))
+    # 年月日だけでソートしているので時刻部分が考慮されない。
+    # 同日内で時刻によるソートが必要になった場合は要調整
   end
 
   def new
     @item = Item.new
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def create
+    # 入力された予約サイト名を確認して処理
+    pre_ordered_site = PreOrderedSite.find_or_create_by(name: params[:item][:pre_ordered_site_name])
+  
+    # アイテム作成処理
     @item = current_user.items.build(item_params)
+    @item.pre_ordered_site_id = pre_ordered_site.id
+  
     if @item.save
       redirect_to items_path, success: 'グッズ情報を登録しました'
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#top"
   resources :users, only: %i[new create]
-  resources :items
+  resources :items, only: %i[index new create show edit destroy]
   
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
