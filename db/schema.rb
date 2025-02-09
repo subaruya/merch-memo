98db5fcd3558d41bb9b505378efdcc2431a08c28
subaruya <s.subaruya@gmail.com>
2025-02-09 13:41:46 +0900

    userテーブルにusernameを追加しビューとコントローラーにも反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_01_100319) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_09_041311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_01_100319) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
